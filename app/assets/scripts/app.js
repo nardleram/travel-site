28b9665cf6287c0562799bc984c5ea8a20251070
nardleram <econosophy@gmail.com>
2019-11-23 11:20:58 +0000

    Completed menu icon functionality

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,9 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
 
+let mobileMenu = new MobileMenu();
+
+// Loads module for live ("hot") module replacement
 if (module.hot) {
     module.hot.accept()
 }
