81692f46d8c64ab7e9ee1123a97d0f3ed79eee97
nardleram <econosophy@gmail.com>
2019-11-24 12:29:39 +0000

    Completed full functionality of sticky header and position-responsive nav items

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,10 +1,12 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 92);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 80);
+let stickyHeader = new StickyHeader()
 
 // Loads module for live ("hot") module replacement
 if (module.hot) {
