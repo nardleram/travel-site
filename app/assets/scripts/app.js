32e4af1a97b115ede973d91cb4670ee385127ee2
nardleram <econosophy@gmail.com>
2019-11-25 13:10:56 +0000

    Added files and functionality for webpack build process

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -4,7 +4,6 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
-
 new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 92);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 80);
